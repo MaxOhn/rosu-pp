c74c7f6e85d22d262499e6192e72a40846a576b1
MaxOhn <ohn.m@hotmail.de>
2022-07-04 13:57:17 +0200

    updated readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 //! A standalone crate to calculate star ratings and performance points for all [osu!](https://osu.ppy.sh/home) gamemodes.
 //!
-//! Conversions between game modes (i.e. "converts") are generally not supported.
-//!
 //! Async is supported through features, see below.
 //!
 //! ## Usage
